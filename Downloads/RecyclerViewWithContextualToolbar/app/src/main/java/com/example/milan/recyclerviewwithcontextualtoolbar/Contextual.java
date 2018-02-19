f62b1e719b935fc501502d1f144a686be76479fb
Your Name <you@example.com>
2018-02-19 15:45:12 +0545

    complete onclickListener for contextual data

---
+++ Downloads/RecyclerViewWithContextualToolbar/app/src/main/java/com/example/milan/recyclerviewwithcontextualtoolbar/Contextual.java
@@ -0,0 +1,14 @@
+package com.example.milan.recyclerviewwithcontextualtoolbar;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by milan on 2/19/18.
+ */
+
+public class Contextual extends AppCompatActivity {
+
+
+
+
+}
